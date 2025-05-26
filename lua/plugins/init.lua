9c30343d42a65b6b821485fea883d2c68e4c9eb3
alekay2200 <alejandropalomino22@gmail.com>
2025-05-26 10:18:40 +0200

    Add git-conflict plugin

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -64,6 +64,7 @@ return {
     end
   },
 
+  -- LazyGit --
   {
     "kdheepak/lazygit.nvim",
     lazy = false,
@@ -87,6 +88,54 @@ return {
       vim.g.lazygit_floating_window_scaling_factor = 1.0
       vim.g.lazygit_floating_window_winblend = 0
       vim.g.lazygit_floating_window_border_chars = {'', '', '', '', '', '', '', ''}
+    end
+  },
+
+  -- Git-conflict --
+  {
+    'akinsho/git-conflict.nvim',
+    dependencies = {
+        'yorickpeterse/nvim-pqf'
+    },
+    lazy = true,
+    version = "*",
+    config = function()
+        require('pqf').setup()
+        require("git-conflict").setup({
+          default_mappings = {
+            ours = 'o',
+            theirs = 't',
+            none = '0',
+            both = 'b',
+            next = 'n',
+            prev = 'p',
+          },
+            default_commands = true,    -- disable commands created by this plugin
+            disable_diagnostics = true, -- This will disable the diagnostics in a buffer whilst it is conflicted
+            list_opener = 'copen',      -- command or function to open the conflicts list
+            highlights = {              -- They must have background color, otherwise the default color will be used
+                incoming = 'DiffAdd',
+                current  = 'DiffText',
+            },
+        })
+
+        -- vim.keymap.set('n', '<leader>gc', ':GitConflictRefresh<CR>', {noremap = true, silent = true})
+        -- vim.keymap.set('n', '<leader>gq', ':GitConflictQf<CR>', {noremap = true, silent = true})
+
+        vim.api.nvim_create_autocmd('User', {
+            pattern = 'GitConflictDetected',
+            callback = function()
+                vim.notify('Conflict detected in file ' .. vim.api.nvim_buf_get_name(0))
+                vim.cmd('LspStop')
+            end
+        })
+
+        vim.api.nvim_create_autocmd('User', {
+            pattern = 'GitConflictResolved',
+            callback = function()
+                vim.cmd('LspRestart')
+            end
+        })
     end
   }
 }
