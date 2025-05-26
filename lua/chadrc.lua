cb4695e12d04939f5d07c51487e8439561f2a187
alekay2200 <alejandropalomino22@gmail.com>
2025-05-26 13:17:15 +0200

    Custom function to set relative file path instead of file name only in the statusline

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -1,6 +1,7 @@
 -- This file needs to have same structure as nvconfig.lua 
 -- https://github.com/NvChad/ui/blob/v3.0/lua/nvconfig.lua
 -- Please read that file to know all available options :( 
+local utils = require("nvchad.stl.utils")
 
 ---@type ChadrcConfig
 local M = {}
@@ -15,10 +16,24 @@ M.base46 = {
 }
 
 -- M.nvdash = { load_on_startup = true }
--- M.ui = {
---       tabufline = {
---          lazyload = false
---      }
---}
+
+-- Modify file function to show relative path of the file instead of only the name
+local customFile = function()
+  local x = utils.file()
+  local icon = x[1]
+  -- local name = x[2]
+  local relative_path = vim.fn.fnamemodify(vim.fn.expand('%'), ':.')
+  local name = " " .. relative_path
+  if relative_path == "" then name = " Empty" end
+  return "%#StText# " .. icon .. name
+end
+
+M.ui = {
+  statusline = {
+    modules = {
+      file = customFile
+    }
+  }
+}
 
 return M
