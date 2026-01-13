1b69092b857a0b3ce3a567e84d3af070d09921b0
alekay2200 <alejandropalomino22@gmail.com>
2026-01-13 12:09:48 +0100

    automatic resize and align function

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -3,6 +3,8 @@ require "nvchad.autocmds"
 local api = vim.api
 local bo = vim.bo
 local opt = vim.opt
+local cmd = vim.cmd
+local fn = vim.fn
 
 -- Buffer Custom commands
 -- Next buffer
@@ -56,6 +58,8 @@ api.nvim_create_user_command(
     { nargs = 0 }
 )
 
+
+-- Disable expandtab option for go files
 api.nvim_create_autocmd({"BufEnter", "BufWinEnter"}, {
     pattern = "*",
     callback = function()
@@ -68,3 +72,46 @@ api.nvim_create_autocmd({"BufEnter", "BufWinEnter"}, {
       end
     end
 })
+
+-- Resize nvimtree if the window was resized
+api.nvim_create_autocmd({ "VimResized" }, {
+  group = api.nvim_create_augroup("NvimTreeResize", { clear = true }),
+  callback = function()
+    local width = vim.go.columns
+    cmd("NvimTreeResize " .. width)
+  end,
+})
+
+
+local function align(opts)
+  local text = opts.args
+  local start_line = fn.line("'<") - 1
+  local end_line = fn.line("'>")
+
+  local lines = api.nvim_buf_get_lines(0, start_line, end_line, false)
+  local new_lines = {}
+
+  local big_index = 0
+  for _, line in ipairs(lines) do
+    local s, _ = string.find(line, text)
+    if s ~= nil and big_index < s then big_index = s end
+    local entry = { line = line, find_index = s }
+    table.insert(new_lines, entry)
+  end
+
+  local modified_lines = {}
+  for _, entry in ipairs(new_lines) do
+    local line = entry["line"]
+    local index = entry["find_index"]
+    local spaces = big_index - index
+    local new = line
+    if spaces > 0 then
+      new = line:sub(1, index - 1) .. string.rep(" ", spaces) .. line:sub(index)
+    end
+    table.insert(modified_lines, new)
+  end
+
+  api.nvim_buf_set_lines(0, start_line, end_line, false, modified_lines)
+end
+
+api.nvim_create_user_command("Align", align, { nargs = 1, range = true })
